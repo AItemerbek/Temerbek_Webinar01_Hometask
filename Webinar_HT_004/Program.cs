b1315b119f553e23d6bcb85590db5ac10789a585
alekseytemerbek <tech.temerbek@gmail.com>
2023-08-02 13:19:19 +0300

    Add Webinar_Hometask_004

---
+++ Webinar_HT_004/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 4: 
+
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Clear();
+
+Console.WriteLine("Добрый день, пользователь! Данная программа позволяет найти наибольшее число из трех вводимых.");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int maxnumber = a;
+
+if(b > maxnumber)
+{
+    maxnumber = b;
+}
+if(c > maxnumber)
+    {
+        maxnumber = c;
+    }
+
+Console.Write($"Из чисел {a}, {b}, {c},  наибольшее число {maxnumber}"); 
